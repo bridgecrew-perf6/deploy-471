305ed3174416a4714947027a23273e86b3e6f2df
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-01-11 22:25:39 +0100

    updates switch

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ switchBtn = document.querySelector('.switch')
 body = document.querySelector('body')
 
 darkBtn.addEventListener('click', () => {
-  body.style.backgroundColor = 'black';
+  body.style.backgroundColor = 'black'
 })
 
 // beigeBtn.addEventListener(event, callback)
 beigeBtn.addEventListener('click', () => {
-  body.style.backgroundColor = 'beige';
+  body.style.backgroundColor = 'beige'
 })
 
 // beigeBtn.addEventListener(event, callback)
 switchBtn.addEventListener('click', () => {
     if (body.style.backgroundColor === "beige") {
-      body.style.backgroundColor === "black"
+      body.style.backgroundColor = "black";
     } else {
       body.style.backgroundColor = "beige";
     }
