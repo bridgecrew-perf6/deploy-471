568e908354ee53523da4eb2266cc1ad2d56ce842
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-01-11 21:08:06 +0100

    various updates

---
+++ script.js
@@ -0,0 +1,27 @@
+// darkBtn = // to target
+darkBtn = document.querySelector('.dark')
+// beigeBtn = // to target
+beigeBtn = document.querySelector('.beige')
+// switchBtn = // to target
+switchBtn = document.querySelector('.switch')
+
+body = document.querySelector('body')
+
+darkBtn.addEventListener('click', () => {
+  body.style.backgroundColor = 'black'
+})
+
+// beigeBtn.addEventListener(event, callback)
+beigeBtn.addEventListener('click', () => {
+  body.style.backgroundColor = 'beige'
+})
+
+// beigeBtn.addEventListener(event, callback)
+switchBtn.addEventListener('click', () => {
+    if (body.style.backgroundColor === "beige") {
+      body.style.backgroundColor = "black" ;
+    } else {
+      body.style.backgroundColor = "beige";
+    }
+  }
+)
